791c91b07bcf7aedd441ef45eb67aa14df469874
robertcroc <solarbang@yahoo.com>
2022-08-02 15:20:35 +0000

    Add subtraction

--- Calculator.java
+++ Calculator.java
@@ -6,6 +6,13 @@ public class Calculator {
 	
 	public double addition(double a, double b) {
 		return a + b;
-	
+	}
+
+	public int subtraction(int a, int b) {
+		return a - b;
+	}
+
+	public double subtraction(double a, double b) {
+		return a - b;
 	}
 }
